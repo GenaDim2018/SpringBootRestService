1a24c880fd82aee323379689a8f79746e94aa7b8
GenaDim2018 <genadim2018@gmail.com>
2024-01-12 12:14:46 +0300

    Initial commit

---
+++ src/main/java/ru/stepiklearning/spring/MainApp.java
@@ -0,0 +1,12 @@
+package ru.stepiklearning.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
+    }
+}
